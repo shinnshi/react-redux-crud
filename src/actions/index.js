e8f3b2898cdbd6968019fe0e4b1f424ee10b425f
shinnshi <shin.nshi3@gmail.com>
2020-03-31 21:26:10 +0900

    implement action and actionCreater

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+const INCREMENT = "INCREMENT"
+const DECREMENT = "DECREMENT"
+export const increment = () => {
+    {
+      type: INCREMENT
+    }
+}
+export const decrement = () => {
+  {
+    type: DECREMENT
+  }
+    
+}
