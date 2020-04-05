4968f76ff647f8e8dd10b9fcca981539b1d34fb2
shinnshi <shin.nshi3@gmail.com>
2020-04-05 23:30:13 +0900

    apply ,material-ui to eventsIndex

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
-import reducer from './reducers'
+import reducer from './reducers';
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
-import EventsShow from './components/events_show'
+import EventsShow from './components/events_show';
 // import registerServiceWorker from './registerServiceWorker';
 const enhancer =
   process.env.NODE_ENV === 'development' ?
@@ -17,16 +18,19 @@ const enhancer =
 const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/events/new" component={EventsNew} />
-        <Route path="/events/:id" component={EventsShow} />
-        <Route exact path="/" component={EventsIndex} />
-        <Route exact path="/events" component={EventsIndex} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
+  <MuiThemeProvider>
+
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/events/new" component={EventsNew} />
+          <Route path="/events/:id" component={EventsShow} />
+          <Route exact path="/" component={EventsIndex} />
+          <Route exact path="/events" component={EventsIndex} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
 // registerServiceWorker();
